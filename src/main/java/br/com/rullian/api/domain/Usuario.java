1a158d7a10aa26407aaa1095e1ce3f80d20a0a58
Rullian Siqueira <rullianjbt@gmail.com>
2023-04-10 20:02:25 -0300

    Projeto Iniciado

--- src/main/java/br/com/rullian/api/domain/Usuario.java
+++ src/main/java/br/com/rullian/api/domain/Usuario.java
@@ -1,4 +1,21 @@
 package br.com.rullian.api.domain;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+
 }
