5bf2de4146865522908b8e3c3d692f32541ccc87
Rullian Siqueira <rullianjbt@gmail.com>
2023-04-10 19:26:40 -0300

    Projeto Iniciado

---
+++ src/test/java/br/com/rullian/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rullian.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
